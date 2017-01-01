e4f201a855551f9f11c5e5d1e2be4bcec679b66b
Arata Tanaka <tarata43@yahoo.co.jp>
2017-01-02 03:21:24 +0900

    入力の行列が意図しない形になってるっぽさ

---
+++ python/mnist/functions.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+    y = y.reshape(1, y.size)
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t])) / batch_size
+
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+        it.iternext()
+
+    return grad
--- python/mnist/simplenetwork.py
+++ python/mnist/simplenetwork.py
@@ -1,4 +1,79 @@
-class SimpleNetwork:
+from collections import OrderedDict
+import numpy as np
+
+from mnist.functions import cross_entropy_error, numerical_gradient
+from mnist.layers import Affine, SoftmaxWithLoss, Relu
 
 
+class SimpleNetWork:
+    def __init__(self, input_size,
+                 hidden_size, output_size,
+                 weight_init_std=0.01
+                 ):
+        self.params = {}
+        self.params['W1'] = weight_init_std * \
+            np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * \
+            np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
 
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(
+            self.params['W1'], self.params['b1']
+        )
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(
+            self.params['W2'], self.params['b2']
+        )
+        self.lastLayer = SoftmaxWithLoss()
+
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        return cross_entropy_error(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        if t.ndim != 1:
+            t = np.argmax(t, axis=1)
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        grads = {}
+
+        grads['W1'] = numerical_gradient(
+            lambda x: self.loss(x, t), self.params['W1']
+        )
+        grads['b1'] = numerical_gradient(
+            lambda x: self.loss(x, t), self.params['b1']
+        )
+        grads['W2'] = numerical_gradient(
+            lambda x: self.loss(x, t), self.params['W2']
+        )
+        grads['b2'] = numerical_gradient(
+            lambda x: self.loss(x, t), self.params['b2']
+        )
+        return grads
+
+    def gradient(self, x, t):
+        self.loss(x, t)
+        dout = 1
+        dout = self.lastLayer.backward(dout)
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        grads = {}
+        grads['W1'] = self.layers['Affine1'].dW
+        grads['b1'] = self.layers['Affine1'].db
+        grads['W2'] = self.layers['Affine2'].dW
+        grads['b2'] = self.layers['Affine2'].db
+        return grads
