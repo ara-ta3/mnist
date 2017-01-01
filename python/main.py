245110bc1514a3b963eef655d90e11226caef113
Arata Tanaka <tarata43@yahoo.co.jp>
2017-01-02 03:56:58 +0900

    Impl learning

--- python/main.py
+++ python/main.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 import pickle
 
 from mnist.dataload import load_mnist
@@ -7,25 +6,43 @@ from mnist.simplenetwork import SimpleNetWork
 
 
 def main():
-    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
-    network = SimpleNetWork(input_size=784, hidden_size=50, output_size=10)
-    x_batch = x_train[:3]
-    t_batch = t_train[:3]
-    grad_numerical = network.numerical_gradient(
-        x_batch, t_batch
+    (x_train, t_train), (x_test, t_test) = load_mnist(
+        normalize=True, one_hot_label=True
     )
-    grad_backprop = network.gradient(
-        x_batch, t_batch
+    network = SimpleNetWork(
+        input_size=784, hidden_size=50, output_size=10
     )
+    iters_num = 10000
+    train_size = x_train.shape[0]
+    batch_size = 100
+    learning_rate = 0.1
+    train_loss_list = []
+    train_acc_list = []
+    test_acc_list = []
+    iter_per_epoch = max(train_size / batch_size, 1)
+    for i in range(iters_num):
+        batch_mask = np.random.choice(train_size, batch_size)
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
 
-    with open('grad_numerical.pkl', mode='wb') as f:
-        pickle.dump(grad_numerical, f)
-
-    for key in grad_numerical.keys():
-        diff = np.average(
-            np.abs(grad_backprop[key] - grad_numerical[key])
+        grad = network.numerical_gradient(
+            x_batch, t_batch
         )
-        print(key + ":" + str(diff))
+
+        for key in ('W1', 'b1', 'W2', 'b2'):
+            network.params[key] -= learning_rate * grad[key]
+
+        loss = network.loss(x_batch, t_batch)
+        train_loss_list.append(loss)
+        if i % iter_per_epoch == 0:
+            train_acc = network.accuracy(x_train, t_train)
+            test_acc = network.accuracy(x_test, t_test)
+            train_acc_list.append(train_acc)
+            test_acc_list.append(test_acc)
+            print(train_acc, test_acc)
+
+    with open('network.pkl', mode='wb') as f:
+        pickle.dump(network, f)
 
 
 if __name__ == '__main__':
